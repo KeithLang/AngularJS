0a5b9dfbf29b4ac751017ae2aaf4e48b600c4b2e
Keith <keithlang@yahoo.com>
2014-11-25 21:04:14 +0000

    next commit

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,15 @@
-var myNameSpace = angular.module('myApp', []);
-myNameSpace.controller('MyController', function MyController($scope) {
+var artistControllers = angular.module('artistControllers', []);
+
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) { $http.get('js/data.json').success(function(data) {
      
-     	$scope.author = {
-     	'name' : 'Keith Lang',
-     	'title' : 'Top Dog',
-     	'company' : 'KLDesigns'
-     	}  
- 	});
+     	$scope.artists = data;
+     	$scope.artistOrder = 'name';
+     });
+}]);
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) { $http.get('js/data.json').success(function(data) {
+     
+     	$scope.artists = data;
+     	$scope.whichItem = $routeParams.itemId;
+     });
+}]);
